1ddeb1ce0811f0263c06c3f590d0c41ad6f1760b
tanverrana <tanversobur@gmail.com>
2022-05-18 00:11:36 +0600

    create user api

--- index.js
+++ index.js
@@ -47,6 +47,11 @@ async function run() {
 
         });
 
+        app.get("/user", async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
         app.put("/user/:email", async (req, res) => {
             const email = req.params.email;
             const user = req.body;
