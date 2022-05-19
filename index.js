cfad573d53b7691def2415becd87b93d1e99119d
tanverrana <tanversobur@gmail.com>
2022-05-19 10:06:06 +0600

    create doctor collection

--- index.js
+++ index.js
@@ -36,12 +36,13 @@ async function run() {
         const serviceCollection = client.db("doctors_portal").collection("services");
         const bookingCollection = client.db("doctors_portal").collection("booking");
         const userCollection = client.db("doctors_portal").collection("users");
+        const doctorCollection = client.db("doctors_portal").collection("doctors");
 
 
 
         app.get("/service", async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).project({ name: 1 });
             const services = await cursor.toArray();
             res.send(services);
 
@@ -145,7 +146,13 @@ async function run() {
             }
             const result = bookingCollection.insertOne(booking);
             res.send({ success: true, result });
-        })
+        });
+
+        app.post("/doctor", async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorCollection.insertOne(doctor);
+            res.send(result);
+        });
 
 
     }
