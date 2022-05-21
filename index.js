acd231e9076747aea83fd2dbf382d5ef620edf0d
tanverrana <tanversobur@gmail.com>
2022-05-21 10:18:36 +0600

    create booking with patient id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -142,6 +142,13 @@ async function run() {
             }
 
         });
+
+        app.get("/booking/:id", verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
+        })
 
 
         app.post("/booking", async (req, res) => {
